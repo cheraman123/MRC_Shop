8b497867785ad8bfbfabfa5c70875d63abc059b1
Reshmalekshmycheraman <cheramandurai68@gmail.com>
2024-11-07 13:21:45 +0530

    initial commit

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','slug','images','description','price','is_active','is_featured','in_stock','on_sale'];
+
+    protected $casts = ['images'=> 'array'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+
+
+    }
+    public function orderitem(){
+        return $this->hasMany(Orderitem::class);
+    }
+}
