ceb9b33737b97cb6c54dfac805829a6a356095ab
Reshmalekshmycheraman <cheramandurai68@gmail.com>
2024-11-30 15:06:08 +0530

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Homepage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\CartPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\SuccessPage;
+use App\Livewire\CancelPage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\ResetPasswordPage;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +28,29 @@ use App\Livewire\Homepage;
 */
 
 Route::get('/',HomePage::class);
+
+Route::get('/categories',CategoriesPage::class);
+
+Route::get('/products',ProductsPage::class);
+
+Route::get('/cart',CartPage::class);
+
+Route::get('/product/{Product}',ProductDetailPage::class);
+
+Route::get('/checkout',CheckoutPage::class);
+
+Route::get('/my-orders',MyOrdersPage::class);
+
+Route::get('/my-orders/{Order}',MyOrderDetailPage::class);
+
+Route::get('/login',LoginPage::class);
+
+Route::get('/register',RegisterPage::class);
+
+Route::get('/forgot',ForgotPasswordPage::class);
+
+Route::get('/reset',ResetPasswordPage::class);
+
+Route::get('/success',SuccessPage::class);
+
+Route::get('/cancel',CancelPage::class);
